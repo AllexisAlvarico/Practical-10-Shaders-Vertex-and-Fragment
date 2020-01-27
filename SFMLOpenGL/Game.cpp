0755b1e2426ef3e407be9013747a459d8e46ab0b
(Student) - Allexis Alvarico <c00242855@ITCARLOW.IE>
2020-01-27 10:30:08 +0000

    The cube has been drawn

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -2,7 +2,7 @@
 
 static bool flip;
 
-Game::Game() : window(VideoMode(800, 600), "OpenGL Cube Vertex and Fragment Shaders")
+Game::Game() : window(sf::VideoMode(800, 600), "OpenGL Cube Vertex and Fragment Shaders")
 {
 
 }
@@ -14,7 +14,7 @@ void Game::run()
 
 	initialize();
 
-	Event event;
+	sf::Event event;
 
 	while (isRunning) {
 
@@ -24,7 +24,7 @@ void Game::run()
 
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
+			if (event.type == sf::Event::Closed)
 			{
 				isRunning = false;
 			}
@@ -41,8 +41,31 @@ typedef struct
 	float color[4];
 } Vertex;
 
-Vertex vertex[3];
-GLubyte triangles[3];
+Vertex vertex[8];
+GLubyte triangles[36];
+
+float vertices[] = { -1.0f, -1.0f, 1.0f,
+						1.0f, -1.0f, 1.0f,
+						1.0f, 1.0f, 1.0f,
+						-1.0f, 1.0f, 1.0f,
+						-1.0f, -1.0f, -1.0f,
+						1.0f, -1.0f, -1.0f,
+						1.0f, 1.0f, -1.0f,
+						-1.0f, 1.0f, -1.0f };
+
+Vector3f m_points[] = { {vertices[0],vertices[1],vertices[2] },
+						{vertices[3],vertices[4],vertices[5] },
+						{vertices[6],vertices[7],vertices[8] },
+						{vertices[9],vertices[10],vertices[11] },
+						{vertices[12],vertices[13],vertices[14] },
+						{vertices[15],vertices[16],vertices[17] },
+						{vertices[18],vertices[19],vertices[20] },
+							{vertices[21],vertices[22],vertices[23] }
+};
+
+
+
+
 
 /* Variable to hold the VBO identifier and shader data */
 GLuint	index, //Index to draw
@@ -57,41 +80,108 @@ GLuint	index, //Index to draw
 void Game::initialize()
 {
 	isRunning = true;
+	for (int i = 0; i < 8; i++)
+	{
+		m_points[i] = Matrix3f::scale3D(50) * m_points[i];
+	}
+	glTranslated(0.0f, 0.0f, -8.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
+	glMatrixMode(GL_MODELVIEW);
+	glEnable(GL_CULL_FACE);
+
 	GLint isCompiled = 0;
 	GLint isLinked = 0;
 
 	glewInit();
 
 	/* Vertices counter-clockwise winding */
-	vertex[0].coordinate[0] = -0.5f;
-	vertex[0].coordinate[1] = -0.5f;
-	vertex[0].coordinate[2] = 0.0f;
+	vertex[0].coordinate[0] = vertices[0];
+	vertex[0].coordinate[1] = vertices[1];
+	vertex[0].coordinate[2] = vertices[2];
 
-	vertex[1].coordinate[0] = -0.5f;
-	vertex[1].coordinate[1] = 0.5f;
-	vertex[1].coordinate[2] = 0.0f;
+	vertex[1].coordinate[0] = vertices[3];
+	vertex[1].coordinate[1] = vertices[4];
+	vertex[1].coordinate[2] = vertices[5];
 
-	vertex[2].coordinate[0] = 0.5f;
-	vertex[2].coordinate[1] = 0.5f;
-	vertex[2].coordinate[2] = 0.0f;
+	vertex[2].coordinate[0] = vertices[6];
+	vertex[2].coordinate[1] = vertices[7];
+	vertex[2].coordinate[2] = vertices[8];
+
+	vertex[3].coordinate[0] = vertices[9];
+	vertex[3].coordinate[1] = vertices[10];
+	vertex[3].coordinate[2] = vertices[11];
+
+	vertex[4].coordinate[0] = vertices[12];
+	vertex[4].coordinate[1] = vertices[13];
+	vertex[4].coordinate[2] = vertices[14];
+
+	vertex[5].coordinate[0] = vertices[15];
+	vertex[5].coordinate[1] = vertices[16];
+	vertex[5].coordinate[2] = vertices[17];
+
+	vertex[6].coordinate[0] = vertices[18];
+	vertex[6].coordinate[1] = vertices[19];
+	vertex[6].coordinate[2] = vertices[20];
+
+	vertex[7].coordinate[0] = vertices[21];
+	vertex[7].coordinate[1] = vertices[22];
+	vertex[7].coordinate[2] = vertices[23];
 
 	vertex[0].color[0] = 0.5f;
 	vertex[0].color[1] = 0.0f;
 	vertex[0].color[2] = 0.5f;
-	vertex[0].color[3] = 1.0f;
+	vertex[0].color[3] = 0.5f;
 
 	vertex[1].color[0] = 0.5f;
-	vertex[1].color[1] = 0.0f;
-	vertex[1].color[2] = 0.5f;
-	vertex[1].color[3] = 1.0f;
+	vertex[1].color[1] = 1.0f;
+	vertex[1].color[2] = 0.3f;
+	vertex[1].color[3] = 0.0f;
 
-	vertex[2].color[0] = 0.5f;
+	vertex[2].color[0] = 0.3f;
 	vertex[2].color[1] = 0.0f;
-	vertex[2].color[2] = 0.5f;
-	vertex[2].color[3] = 1.0f;
+	vertex[2].color[2] = 1.0f;
+	vertex[2].color[3] = 0.5f;
+
+	vertex[3].color[0] = 0.4f;
+	vertex[3].color[1] = 0.6f;
+	vertex[3].color[2] = 0.4f;
+	vertex[3].color[3] = 0.5f;
+
+	vertex[4].color[0] = 0.5f;
+	vertex[4].color[1] = 1.0f;
+	vertex[4].color[2] = 0.5f;
+	vertex[4].color[3] = 0.0f;
+
+	vertex[5].color[0] = 1.2f;
+	vertex[5].color[1] = 0.2f;
+	vertex[5].color[2] = 0.4f;
+	vertex[5].color[3] = 0.5f;
+
+	vertex[6].color[0] = 1.0f;
+	vertex[6].color[1] = 0.6f;
+	vertex[6].color[2] = 0.5f;
+	vertex[6].color[3] = 0.0f;
+
+	vertex[7].color[0] = 0.6f;
+	vertex[7].color[1] = 0.7f;
+	vertex[7].color[2] = 0.3f;
+	vertex[0].color[3] = 0.5f;
 
 	/*Index of Poly / Triangle to Draw */
 	triangles[0] = 0;   triangles[1] = 1;   triangles[2] = 2;
+	triangles[3] = 2;   triangles[4] = 3;   triangles[5] = 0;
+	triangles[6] = 2;   triangles[7] = 6;   triangles[8] = 3;
+	triangles[9] = 6;   triangles[10] = 7;   triangles[11] = 3;
+	triangles[12] = 7;   triangles[13] = 6;   triangles[14] = 5;
+	triangles[15] = 5;   triangles[16] = 4;   triangles[17] = 7;
+	triangles[18] = 5;   triangles[19] = 0;   triangles[20] = 4;
+	triangles[21] = 5;   triangles[22] = 1;   triangles[23] = 0;
+	triangles[24] = 1;   triangles[25] = 5;   triangles[26] = 6;
+	triangles[27] = 6;   triangles[28] = 2;   triangles[29] = 1;
+	triangles[30] = 4;   triangles[31] = 0;   triangles[32] = 3;
+	triangles[33] = 3;   triangles[34] = 7;   triangles[35] = 4;
 
 	/* Create a new VBO using VBO id */
 	glGenBuffers(1, &vbo);
@@ -100,12 +190,12 @@ void Game::initialize()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
 	/* Upload vertex data to GPU */
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 7, vertex, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 36, vertex, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glGenBuffers(1, &index);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte) * 3, triangles, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte) * 36, triangles, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	/* Vertex Shader which would normally be loaded from an external file */
@@ -204,28 +294,21 @@ void Game::update()
 			flip = false;
 	}
 
-	if (flip)
-	{
-		rotationAngle += 0.005f;
 
-		if (rotationAngle > 360.0f)
-		{
-			rotationAngle -= 360.0f;
-		}
-	}
+	cubeRotation();
 
-	//Change vertex data
-	vertex[0].coordinate[0] += -0.0001f;
-	vertex[0].coordinate[1] += -0.0001f;
-	vertex[0].coordinate[2] += -0.0001f;
+	////Change vertex data
+	//vertex[0].coordinate[0] += -0.0001f;
+	//vertex[0].coordinate[1] += -0.0001f;
+	//vertex[0].coordinate[2] += -0.0001f;
 
-	vertex[1].coordinate[0] += -0.0001f;
-	vertex[1].coordinate[1] += -0.0001f;
-	vertex[1].coordinate[2] += -0.0001f;
+	//vertex[1].coordinate[0] += -0.0001f;
+	//vertex[1].coordinate[1] += -0.0001f;
+	//vertex[1].coordinate[2] += -0.0001f;
 
-	vertex[2].coordinate[0] += -0.0001f;
-	vertex[2].coordinate[1] += -0.0001f;
-	vertex[2].coordinate[2] += -0.0001f;
+	//vertex[2].coordinate[0] += -0.0001f;
+	//vertex[2].coordinate[1] += -0.0001f;
+	//vertex[2].coordinate[2] += -0.0001f;
 
 #if (DEBUG >= 2)
 	DEBUG_MSG("Update up...");
@@ -249,7 +332,7 @@ void Game::render()
 
 	/*	As the data positions will be updated by the this program on the
 		CPU bind the updated data to the GPU for drawing	*/
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 3, vertex, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 36, vertex, GL_STATIC_DRAW);
 
 	/*	Draw Triangle from VBO	(set where to start from as VBO can contain
 		model components that 'are' and 'are not' to be drawn )	*/
@@ -263,9 +346,148 @@ void Game::render()
 	glEnableVertexAttribArray(positionID);
 	glEnableVertexAttribArray(colorID);
 
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE, (char*)NULL + 0);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, (char*)NULL + 0);
 
 	window.display();
+
+}
+
+void Game::cubeRotation()
+{
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::rotationX(rotationAngle) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::rotationX(-rotationAngle) * m_points[i];
+		}
+	}
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::rotationY(-rotationAngle) * m_points[i];
+		}
+	}
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::rotationY(rotationAngle) * m_points[i];
+		}
+	}
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::rotationZ(rotationAngle) * m_points[i];
+		}
+	}
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::rotationZ(-rotationAngle) * m_points[i];
+		}
+	}
+
+
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::scale3D(100.05) * m_points[i];
+		}
+	}
+
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3f::scale3D(99.95) * m_points[i];
+		}
+	}
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			float temp = m_points[i].m_z;
+			m_points[i].m_z = 1;
+			m_points[i] = Matrix3f::translation(0.001f, 0.0f) * m_points[i];
+			m_points[i].m_z = temp;
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			float temp = m_points[i].m_z;
+			m_points[i].m_z = 1;
+			m_points[i] = Matrix3f::translation(-0.001f, 0.0f) * m_points[i];
+			m_points[i].m_z = temp;
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			float temp = m_points[i].m_z;
+			m_points[i].m_z = 1;
+			m_points[i] = Matrix3f::translation(0.0f, 0.001f) * m_points[i];
+			m_points[i].m_z = temp;
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			float temp = m_points[i].m_z;
+			m_points[i].m_z = 1;
+			m_points[i] = Matrix3f::translation(0.0f, -0.001f) * m_points[i];
+			m_points[i].m_z = temp;
+		}
+	}
+
+
+
+
+	for (int i = 0, j = 0; i < 8, j < 24; i++)
+	{
+		vertex[i].coordinate[0] = m_points[i].m_x;
+		j++;
+		vertex[i].coordinate[1] = m_points[i].m_y;
+		j++;
+		vertex[i].coordinate[2] = m_points[i].m_z;
+		j++;
+	}
+
+
+
 
 }
 
--- SFMLOpenGL/Game.h
+++ SFMLOpenGL/Game.h
@@ -9,12 +9,8 @@
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
 
-#include <Vector3.h>
-#include <Matrix3.h>
-
-using namespace std;
-using namespace sf;
-using namespace gpp;
+#include <Vector3f.h>
+#include <Matrix3f.h>
 
 class Game
 {
@@ -23,17 +19,18 @@ public:
 	~Game();
 	void run();
 private:
-	Window window;
+	sf::Window window;
 	bool isRunning = false;
 	void initialize();
 	void update();
 	void render();
 	void unload();
+	void cubeRotation();
 
-	Clock clock;
-	Time elapsed;
+	sf::Clock clock;
+	sf::Time elapsed;
 
-	float rotationAngle = 0.0f;
+	float rotationAngle = 0.05f;
 };
 
 #endif
